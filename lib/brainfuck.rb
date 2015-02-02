a33de459f227d01f835e34217940dfe34e4af624
Dylan Conrad Johnson <dylanthevillain@gmail.com>
2015-02-02 00:49:54 -0600

    all but looping.

--- lib/brainfuck.rb
+++ lib/brainfuck.rb
@@ -14,7 +14,11 @@ def Main()
 	else
 		if ARGV[0] == "-t"
 			fileReader = Input::TextSourceFile.new(ARGV[1])
-			puts fileReader.ReadFile
+			brainfuckString = fileReader.ReadFile
+			interpreter = Interpreter::BrainFuckInterpreter.new()
+			interpreter.ProcessTokens(brainfuckString) do |output|
+				puts output
+			end
 		elsif ARGV[0] == "-i"
 			puts Usage()
 		else
--- lib/interpreter/interpreter.rb
+++ lib/interpreter/interpreter.rb
@@ -1,24 +1,66 @@
 module Interpreter
-	def self.ProcessTokens(brainFuckString)
-		brainFuckString.each_char do |token|
-			case token
-			when '>'
-				yield '>'
-			when '<'
-				yield '<'
-			when '+'
-				yield '+'
-			when '-'
-				yield '-'
-			when '.'
-				yield '.'
-			when '.'
-				yield '.'
-			when '['
-				yield '['
-			when ']'
-				yield ']'
+	class BrainFuckInterpreter
+		def initialize()
+			@byteArray = [0]
+			@pointer = 0
+		end
+
+		def ProcessTokens(brainFuckString)
+			brainFuckString.each_char do |token|
+				case token
+				when '>'
+					IncrementPointer()
+				when '<'
+					DecrementPointer()
+				when '+'
+					IncrementPointerLocation()
+				when '-'
+					DecrementPointerLocation()
+				when '.'
+					yield GetPointerLocation()
+				when ','
+					GetByte()
+				when '['
+					yield '['
+				when ']'
+					yield ']'
+				else
+					raise TypeError
+				end
 			end
+		end
+
+		def IncrementPointer()
+			if @pointer == @byteArray.length - 1
+				@byteArray << 0
+			end
+			@pointer += 1
+		end
+
+		def DecrementPointer()
+			if @pointer == 0
+				raise IndexError
+			else
+				@pointer -= 1
+			end
+		end
+
+		def IncrementPointerLocation()
+			@byteArray[@pointer] += 1
+		end
+
+		def DecrementPointerLocation()
+			@byteArray[@pointer] -= 1
+		end
+
+		def GetPointerLocation()
+			pointerLocation = @byteArray[@pointer].chr
+			return pointerLocation
+		end
+
+		def GetByte()
+			input = STDIN.gets
+			@byteArray[@pointer] = input.ord
 		end
 	end
 end
